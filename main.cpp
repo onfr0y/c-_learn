55416ac2beb2c77decd5f1ce306e26fc3a1af076
Kittisak 'Froy' Porkha <kittijustsaid@gmail.com>
2026-05-02 17:37:20 +0700

    learning c++ day1

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+int main() {
+    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+    int age = 21;
+    int year = 2024;
+    int days = 7.5;
+
+    std::cout << days << '\n';
+    std::cout << year << '\n';
+
+
+
+}
