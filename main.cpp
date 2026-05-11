cdeaa5d0c0c3bd5eef732b5667ed0df9dc743d31
Kittisak 'Froy' Porkha <kittijustsaid@gmail.com>
2026-05-11 14:35:30 +0700

    update

--- main.cpp
+++ main.cpp
@@ -3,23 +3,65 @@
 
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void printMyNumbers(int num) {
 
-    std::cout << "This is my Number: " << num << '\n';
-}
 
 
 int main() {
-    printMyNumbers( 300);
-    int energy = 100;
-    while (energy > 0) {
-        std::cout << "Still running..." << std::endl;
-        energy--;
-    }
 
-    int choice;
-    do {
-        std::cout << "Enter 0 to exit:";
-        std::cin >> choice;
-    } while (choice != 0);
+    int age;
+
+    /*int age;
+
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+
+    if (age >= 18) {
+        std::cout << "You are valid." ;
+    }   else if (age < 18) {
+        std::cout << "You are kid, cunt";
+    } else {
+        std::cout << "You are not valid.";
+    } */
+
+
+    // grade account
+/*
+    char grade;
+
+
+    // userinput
+    std::cout << "What is your grade: ";
+    std::cin >> grade; */
+ /*
+    switch (grade) {
+        case "A":
+            std::cout << "You got a nice grade!!";
+            break;
+        case "B":
+            std::cout << 'You are doing ok';
+            break;
+        case "C":
+            std::cout << 'You are doing ok';
+            break;
+        case "D":
+            std::cout << "You are doing ok";
+            break;
+        default:
+            std::cout << "you are stupid";
+        */
+
+    // ternary operator ?
+
+   // int grade = 3;
+
+    //grade >= 60 ? std::cout << "You pass": std::cout << "You fail!";
+
+    int number = 9;
+
+
+
+
+
+    return 0;
+
 }
