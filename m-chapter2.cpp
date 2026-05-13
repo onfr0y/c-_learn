a20f0f57eac9fdd00f29a698f320be07bba05366
Kittisak 'Froy' Porkha <kittijustsaid@gmail.com>
2026-05-13 14:33:55 +0700

    update

--- m-chapter2.cpp
+++ m-chapter2.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 
-// void doPrint() {
-    // std::cout << "this is doPrint function";
-// }
-// void doB() {
-    // std::cout << "in DoB()\n";
-// }
-// void doA() {
-    // std::cout << "starting doa \n";
-    // doB();
-    // std::cout << "ending doa \n";
-// }
+void doPrint() {
+    std::cout << "this is doPrint function";
+}
+void doB() {
+    std::cout << "in DoB()\n";
+}
+void doA() {
+    std::cout << "starting doa \n";
+    doB();
+    std::cout << "ending doa \n";
+}
 int main() {
-    // std::cout << "Chapter 2 Practice Program" << std::endl;
-    // You can start writing your code here
-    // std::cout << "Starting main()" << std::endl;
-    // doPrint();
-    // std::cout << "ending main()\n";
-    // std::cout << "in main()\n";
-    // doA();
-    // std::cout << "ending main\n";
+    std::cout << "Chapter 2 Practice Program" << std::endl;
+    You can start writing your code here
+    std::cout << "Starting main()" << std::endl;
+    doPrint();
+    std::cout << "ending main()\n";
+    std::cout << "in main()\n";
+    doA();
+    std::cout << "ending main\n";
     
     return 0;
 }
