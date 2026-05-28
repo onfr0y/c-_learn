97538ce37993372f3484cab90a21bdb2947c2789
Kittisak 'Froy' Porkha <kittijustsaid@gmail.com>
2026-05-28 14:26:33 +0700

    update learning enum

--- learncpp.cpp
+++ learncpp.cpp
@@ -36,25 +36,15 @@
 // int num{getValueFromUser()};
 
 // std::cout << num << " dobuled is: " << num * 2 << '\n';
-// }
-
-// if we want it -- output line -- into its own function
-
-// void printDouble () {
-
-//   int num{getValueFromUser()};
-
-//   std::cout << num << " dobuled is: " << num * 2 << '\n';
-// }
-
-// int main () {
-//   int num {getValueFromUser()};
+// } // if we want it -- output line -- into its own function // void
+// printDouble () { //   int num{getValueFromUser()}; //   std::cout << num << "
+// dobuled is: " << num * 2 << '\n'; // } // int main () { //   int num
+// {getValueFromUser()};
 
 //   printDouble();
 // }
 
 // 2.5 -- introduction to local scope
-
 // int add(int x, int y)
 //  {
 //   int z{x + y};
@@ -72,13 +62,31 @@
 //   return 0;
 // }
 
-// 2.7 -- forward declarations and definitions
-int add(int x, int y);
+// // 2.7 -- forward declarations and definitions
+// int add(int x, int y);
+
+// int main() {
+//   std::cout << "The sum of 3 & 4 is: " << add(3, 4) << '\n';
+//   return 0;
+// }
+
+// int add(int x, int y) { return x + y; }
+// it gonna be error so we need to fix by this; forward declaration;
+
+//  2.7 Introduction to object-orie
+enum class TrafficLight { Red, Yellow, Green };
 
 int main() {
-  std::cout << "The sum of 3 & 4 is: " << add(3, 4) << '\n';
-  return 0;
-}
+    // Usage
+    TrafficLight mySignal = TrafficLight::Red;
 
-int add(int x, int y) { return x + y; }
-// it gonna be error so we need to fix by this; forward declaration;
+    // This will fail (Type safety):
+    // if (mySignal == 0) { ... }
+
+    // Explicit cast is required if you need the integer value:
+    int value = static_cast<int>(mySignal);
+
+    std::cout << "TrafficLight Red integer value: " << value << '\n';
+    
+    return 0;
+}
