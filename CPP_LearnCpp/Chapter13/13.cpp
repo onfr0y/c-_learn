12f3b4f43b85fc26ed47af6cbcf621309ba32495
Kittisak 'Froy' Porkha <kittijustsaid@gmail.com>
2026-05-28 14:36:33 +0700

    update yall

---
+++ CPP_LearnCpp/Chapter13/13.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// using Length = int;
+// int main() {
+//   Length x{5};
+//   std::cout << x << '\n';
+//   return 0;
+// }
+
+// 13.2 Unscoped enumerations
+int main() {
+
+  int appleColour{0};
+  int shirtColour{1};
+
+  return 0;
+}
